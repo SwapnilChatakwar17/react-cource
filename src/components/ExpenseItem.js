9d8459c3c28172e5a54faf81e89d63528068e9a4
SwapnilChatakwar17 <swapnilchatakwar1997@gmail.com>
2022-04-28 11:33:43 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import ExpenseData from './ExpenseData';
+import Card from './Card';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+  return (
+    <Card className='expense-item'>
+      <ExpenseData date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
